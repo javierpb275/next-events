3c9fb361568a086caa7f562fa7d429af390470f2
javierpb275 <javierpb275@gmail.com>
2021-09-03 16:45:42 +0200

    styled even-item and event-list components.

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import classes from './event-item.module.css';
 
 const EventItem = (props) => {
     const {title, image, date, location, id} = props;
@@ -10,19 +11,19 @@ const EventItem = (props) => {
     const theAddress = location.replace(', ', '\n');//replaced ', ' for /n (line break)
     const exploreLink = `/events/${id}`;
     return(
-        <li>
+        <li className={classes.item}>
             <img src={'/' + image} alt={title}/>
-            <div>
-                <div>
+            <div className={classes.content}>
+                <div className={classes.summary}>
                     <h2>{title}</h2>
-                    <div>
+                    <div className={classes.date}>
                         <time>{theDate}</time>
                     </div>
-                    <div>
+                    <div className={classes.address}>
                         <address>{theAddress}</address>
                     </div>
                 </div>
-                <div>
+                <div className={classes.actions}>
                     <Link href={exploreLink}>Explore Event</Link>
                 </div>
             </div>
