50966448349e6743025dff54c22ef92b1765692d
javierpb275 <javierpb275@gmail.com>
2021-09-04 14:17:59 +0200

    Adding a Filter Form for Filtering Events: - created events-search component for filtering events - added events-search to the AllEventsPage. - render either <a> or <button> in Button component depending on props received

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,15 @@
 import {getAllEvents} from '../../dummy-data';
 import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+import { Fragment } from 'react';
 
 const AllEventsPage = () => {
   const events = getAllEvents();
   return (
-    <div>
+    <Fragment>
+      <EventsSearch/>
       <EventList items={events}/>
-    </div>
+    </Fragment>
   );
 };
 
