4135fd5ea0d2f02c362a2e7b1d813ccbcef312f8
javierpb275 <javierpb275@gmail.com>
2021-09-07 17:51:57 +0200

    working on the all venets pages

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,12 @@
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { Fragment } from "react";
 import { useRouter } from "next/router";
 
-const AllEventsPage = () => {
+const AllEventsPage = (props) => {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   const findEventsHandler = (year, month) => {
     const fullPath = `/events/${year}/${month}`;
@@ -20,5 +20,15 @@ const AllEventsPage = () => {
     </Fragment>
   );
 };
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
+}
 
 export default AllEventsPage;
