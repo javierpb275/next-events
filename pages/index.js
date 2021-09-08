1ea648d84dfb7504a53e9fb2015721b951b0159c
javierpb275 <javierpb275@gmail.com>
2021-09-08 14:22:03 +0200

    Configuring the head Content: - Added head in the HomePage with title and meta

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+import Head from "next/head";
 
 const HomePage = (props) => {
   return (
     <div>
+      <Head>
+        <title>NextEvents</title>
+        <meta name="description" content="Find a lot of events!" />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
