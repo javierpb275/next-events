9eebe9f8bd8fc9c9a8cdf27925f49c158db1d7fd
javierpb275 <javierpb275@gmail.com>
2021-09-13 20:03:18 +0200

    created notification component and styles and displayed in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 import Head from "next/head";
+import Notification from "../components/ui/notification";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is a test." status="error" />
     </Layout>
   );
 }
