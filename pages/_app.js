70477de79817d7c25f8926276a02906eb4057a6b
javierpb275 <javierpb275@gmail.com>
2021-09-14 16:01:02 +0200

    Adding Context State

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,24 @@ import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 import Head from "next/head";
 import Notification from "../components/ui/notification";
+import { NotificationContextProvider } from "../store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name="description" content="NextJS Events" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-      <Notification title="Test" message="This is a test." status="error" />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="NextJS Events" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <Notification title="Test" message="This is a test." status="error" />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
---
+++ store/notification-context.js
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+
+const NotificationContext = createContext({
+  notification: null,
+  showNotification: (notificationData) => {},
+  hideNotification: () => {},
+});
+
+export const NotificationContextProvider = (props) => {
+  const [activeNotification, setActiveNotification] = useState();
+
+  const showNotificationHandler = (notificationData) => {
+    setActiveNotification(notificationData);
+  };
+
+  const hideNotificationHandler = () => {
+    setActiveNotification(null);
+  };
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+};
+
+export default NotificationContext;
